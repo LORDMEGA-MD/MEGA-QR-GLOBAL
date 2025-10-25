fe391f1bb8ec6045ba7a76a0e1daa9ee44ae1e8d
LORD-MEGA MD <ttawmega@gmail.com>
2025-10-25 19:55:35 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,14 @@
 const express = require('express');
-const path = require('path');
 const pairRouter = require('./pair');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Serve static files (CSS, JS, HTML)
-app.use(express.static(path.join(__dirname, '/')));
-
-// Pairing API route
+// Pairing route
 app.use('/pair', pairRouter);
-
-// Serve the pairing HTML page
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'pair.html'));
-});
 
 // Start server
 app.listen(PORT, () => {
-    console.log(`🚀 Server running on http://localhost:${PORT}`);
+    console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Open http://localhost:${PORT} to scan QR`);
 });
