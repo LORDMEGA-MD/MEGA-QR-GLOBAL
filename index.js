41523e9a44958d0200d837d5f2bc9c6edf035735
LORD-MEGA MD <ttawmega@gmail.com>
2025-10-25 20:38:24 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 const express = require('express');
 const app = express();
-const pairRouter = require('./pair'); // make sure pair.js is in the same folder
-
+const pairRouter = require('./pair'); // pair.js in same folder
 const PORT = process.env.PORT || 3000;
 
-// Mount the pair router at root
 app.use('/', pairRouter);
 
-// Start server
 app.listen(PORT, () => {
     console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
